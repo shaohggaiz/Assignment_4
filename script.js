3a7897b307ac64e32df2fce34f842e62192cc034
shohag gazi <shohaggazi0331@gmail.com>
2025-03-02 23:48:23 +0600

    complear Task

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let btn = document.querySelectorAll('.main-btn');
-
+let totalBtn = btn.length;
+let btnIncrease = 0;
 let p = document.querySelector('.para').previousElementSibling
-
-
+let discript = document.getElementById('discript');
+let history = document.querySelector('.history');
+let randomeColor = document.querySelector('#randomeColor');
 for(let i of btn) {
     i.addEventListener('click', (e)=> {
         console.log(i)
@@ -12,19 +14,47 @@ for(let i of btn) {
         // alert(event)
         let paragaraph = `You have completed the task ${event} at ${updateClock()}`
         console.log(paragaraph)
-        let discript = document.getElementById('discript');
         discript.innerHTML +=  `
         <p>
         ${paragaraph}
-        </p>`
-        let task = document.querySelector('.task');
-        
+        </p>`;
+            btnIncrease++;
+            if(btnIncrease === totalBtn) {
+                alert('congrates!!! You have completed all Current task')
+            }
+        increase()
     })
 }
+let task = document.querySelector('.task');
+let increaseT = document.querySelector('.increase');
+let count1 = 6;
+let count2 = 25;
+function increase() {
+    count2++;
+   count1--;
+   task.innerText = count1;
+   increaseT.innerText = count2;
+}
+
+
 function updateClock() {
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
     return `${hours}:${minutes}:${seconds} PM`
+}
+
+history.addEventListener('click', ()=> {
+    discript.innerHTML = ''
+})
+randomeColor.addEventListener('click', ()=> {
+    document.body.style.backgroundColor = randomeColors();
+})
+console.log()
+function randomeColors() {
+    let color1 = Math.floor(Math.random() * 255);
+    let color2 = Math.floor(Math.random() * 255);
+    let color3 = Math.floor(Math.random() * 255);
+    return `rgba(${color1} ${color2} ${color3})`
 }
